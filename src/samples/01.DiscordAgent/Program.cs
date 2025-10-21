a83e9dc092112ec09cf04343fa27db6b64282dbb
Autumn Wyborny <autumnwyborny@witchfolio.com>
2025-10-20 19:34:10 -0700

    Added initial sample, an E2E experience where you can message ChatGPT from discord.

---
+++ src/samples/01.DiscordAgent/Program.cs
@@ -0,0 +1,34 @@
+using Coven.Agents.OpenAI;
+using Coven.Chat.Discord;
+using Coven.Core;
+using Coven.Core.Builder;
+using DiscordAgent;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+// Configuration
+DiscordClientConfig discordConfig = new()
+{
+    BotToken = "", // set your Discord bot token
+    ChannelId = 0   // set your channel id
+};
+
+OpenAIClientConfig openAiConfig = new()
+{
+    ApiKey = "",            // set your OpenAI API key
+    Model = "gpt-5-2025-08-07" // choose the model
+};
+
+// Register DI
+HostApplicationBuilder builder = Host.CreateApplicationBuilder(args);
+builder.Services.AddLogging(b => b.AddConsole());
+builder.Services.AddDiscordChat(discordConfig);
+builder.Services.AddOpenAIAgents(openAiConfig);
+builder.Services.BuildCoven(c => c.MagikBlock<Empty, Empty, RouterBlock>().Done());
+
+IHost host = builder.Build();
+
+// Execute ritual
+ICoven coven = host.Services.GetRequiredService<ICoven>();
+await coven.Ritual<Empty, Empty>(new Empty());
